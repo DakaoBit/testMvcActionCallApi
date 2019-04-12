a835d5020a1b6db8a29cd478b9fd0f8a8b462418
DakaoBit <oasis.wang1016@gmail.com>
2019-04-12 11:31:32 +0800

    加入專案檔案。

---
+++ testMvcActionCallApi/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using testMvcActionCallApi.Models;
+
+namespace testMvcActionCallApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            List<Product> product = new List<Product>();
+
+            string apiUrl = "http://localhost:57797/api/Product/Get";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    product = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Product>>(data);
+                }
+            }
+            return View(product);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ testMvcActionCallApi/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using testMvcActionCallApi.Models;
+
+namespace testMvcActionCallApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        List<Product> product;
+
+        public ProductController()
+        {
+            product = new List<Product>();
+
+            product.Add(new Product() { id = 1, name = "apple", price = 10 });
+            product.Add(new Product() { id = 2, name = "banana", price = 20 });
+            product.Add(new Product() { id = 3, name = "cclemon", price = 30 });
+        }
+
+        [Route("api/Product/Get")]
+        public IEnumerable<Product> Get()
+        {
+           return product;
+        }
+    }
+}
